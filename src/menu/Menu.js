7f2178fd0106a37a277ef54ecc8c73abb383fb6f
cooljasonmelton <cooljasonmelton>
2020-08-13 21:20:03 -0500

    set up file structure and started on menu

---
+++ src/menu/Menu.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Menu.css';
+
+const Menu = () => {
+
+    const navItemNames = [
+        "home",
+        "projects",
+        "contact",
+        "blog"
+    ]
+
+
+    return (
+        <div className="Menu">
+    
+        </div>
+    );
+}
+
+export default Menu;
