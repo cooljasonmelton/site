2411da8cd4d72e7c531ad4bf8986cfce78d8112e
cooljasonmelton <cooljasonmelton>
2020-08-13 21:53:05 -0500

    creating menu items and styling

--- src/menu/Menu.js
+++ src/menu/Menu.js
@@ -3,17 +3,26 @@ import './Menu.css';
 
 const Menu = () => {
 
-    const navItemNames = [
+    const menuItemNames = [
         "home",
         "projects",
         "contact",
         "blog"
     ]
 
+    const renderMenuItems = () => {
+        return menuItemNames.map(name=>{
+            return(
+            <li className="menu-item">{name}</li>
+            )
+        })
+    }
 
     return (
-        <div className="Menu">
-    
+        <div className="Menu cfb">
+            <ul className="menu-item-container">
+                {renderMenuItems()}
+            </ul>   
         </div>
     );
 }
